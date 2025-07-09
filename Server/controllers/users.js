27ed1fc64b8279496d1e54eba165afa080c65b5c
arpanam11 <arpanameshram11@gmail.com>
2025-07-09 18:29:39 +0530

    comment

--- Server/controllers/users.js
+++ Server/controllers/users.js
@@ -1,10 +1,10 @@
 import {v4 as uuid} from "uuid";
 let users = [];
-
+// Get all users
 export const getUsers= (req,res)=>{
     res.send(users);
 }
-
+// Create a user
 export const createUser = (req,res)=>{
 const user= req.body;
 
@@ -12,7 +12,7 @@ users.push({ ...user , id: uuid() })
 res.send("User Added Successfully")
 
 };
-
+//get single user with id
 export const getUser = (req,res)=>{
     const singleUser = users.filter((user) => user.id === req.params.id);
     res.send(singleUser);
